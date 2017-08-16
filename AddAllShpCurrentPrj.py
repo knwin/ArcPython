773cd65cb11fc3de4f41b81ac22853960e04d01a
knwin <knwin@users.noreply.github.com>
2017-08-16 07:45:21 +0000

    Update AddAllShpCurrentPrj.py

--- AddAllShpCurrentPrj.py
+++ AddAllShpCurrentPrj.py
@@ -1,19 +1,25 @@
 #add all shp in subdirectory into current project in arcmap
+#Kyaw Naing Win, OneMap GIS Program Manager
+# #---instruction----
+# (1)type mydir = in python windows
+# (2)from ArcCatalogue windows, drag main folder in to python windows after mydir =
+# copy and paste below codes into python windows
+# press enter twice
+
 import arcpy,csv,os  
   
-mydir = #drag and drop a folder from ArcCatalogue here
-# better copy mydir = part into script window first and add path with drag and drop from catalogue
+#mydir = #drag and drop a folder from ArcCatalogue
 
 # listing all files in subdirectoires 
 #   ---below code borrowed from https://goo.gl/g6Mihd
-def list_files(dir):                                                                                                  
+def list_files(dir):
     r = []                                           
     subdirs = [x[0] for x in os.walk(dir)]
     for subdir in subdirs:              
         files = os.walk(subdir).next()[2]                  
-        if (len(files) > 0):                                                        
-            for file in files:                                                   
-                r.append(subdir+ '\\' + file)                                       
+        if (len(files) > 0):
+            for file in files:
+                r.append(subdir+ '\\' + file)
     return r
 
 # #--access current project and data frame
@@ -25,3 +31,8 @@ for file in list_files(mydir):
   if file.endswith(".shp"):
     lyr = arcpy.mapping.Layer(file)
     arcpy.mapping.AddLayer(df, lyr, "AUTO_ARRANGE")
+
+#clean up
+arcpy.RefreshActiveView()
+arcpy.RefreshTOC()
+del mxd, df, lyr, mydir, file, list_files
